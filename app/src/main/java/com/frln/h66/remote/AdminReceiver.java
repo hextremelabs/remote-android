fee731867069b537b222718ece1aa5f22fef4365
Akapo Damilola F <fdamilola@gmail.com>
2017-01-31 18:30:14 +0100

    More stuff

--- app/src/main/java/com/frln/h66/remote/AdminReceiver.java
+++ app/src/main/java/com/frln/h66/remote/AdminReceiver.java
@@ -3,7 +3,6 @@ package com.frln.h66.remote;
 import android.app.admin.DeviceAdminReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.widget.Toast;
 
 /**
@@ -12,16 +11,6 @@ import android.widget.Toast;
  */
 
 public class AdminReceiver extends DeviceAdminReceiver {
-
-
-    static SharedPreferences getSamplePreferences(Context context) {
-        return context.getSharedPreferences(
-                DeviceAdminReceiver.class.getName(), 0);
-    }
-
-    static String PREF_PASSWORD_QUALITY = "password_quality";
-    static String PREF_PASSWORD_LENGTH = "password_length";
-    static String PREF_MAX_FAILED_PW = "max_failed_pw";
 
     void showToast(Context context, CharSequence msg) {
         Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
@@ -34,27 +23,27 @@ public class AdminReceiver extends DeviceAdminReceiver {
 
     @Override
     public CharSequence onDisableRequested(Context context, Intent intent) {
-        return "This is an optional message to warn the user about disabling.";
+        return "Wadssdsdsds ds,d sd]s, fs ,fs";
     }
 
     @Override
     public void onDisabled(Context context, Intent intent) {
-        showToast(context, "Remote Admin: disabled");
+
     }
 
     @Override
     public void onPasswordChanged(Context context, Intent intent) {
-        showToast(context, "Remote Admin: pw changed");
+
     }
 
     @Override
     public void onPasswordFailed(Context context, Intent intent) {
-        showToast(context, "Remote Admin: pw failed");
+
     }
 
     @Override
     public void onPasswordSucceeded(Context context, Intent intent) {
-        showToast(context, "Remote Admin: pw succeeded");
+            //Something here about stealing passwords. I should remind myself later.
     }
 
 }
